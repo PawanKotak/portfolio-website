4f1c8f900c211d40fb880bd7e5db08d88f5be979
Pawan Kotak <kotakpawan@gmail.com>
2022-04-17 01:00:59 +0530

    portfolio website development

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import centerimage from "./assets/remy_unsplash.jpg";
+import Slide from "react-reveal/Slide";
+import react from "react";
+
+import {
+  FaGithub,
+  FaTwitterSquare,
+  FaLinkedin,
+  FaJs,
+  FaNode,
+  FaCss3,
+  FaReact,
+  FaRegEnvelope,
+} from "react-icons/fa";
 
 function App() {
+  const developerName = "Pawan Kotak";
+  const developerSubTitle = "Frontend Developer /  Web Developer";
+  const developerSkills = [
+    <FaReact></FaReact>,
+    <FaJs></FaJs>,
+    <FaNode></FaNode>,
+    <FaCss3></FaCss3>,
+  ];
+  const developerSocials = [
+    { icon: <FaGithub></FaGithub>, href: "https://github.com/PawanKotak" },
+    {
+      icon: <FaLinkedin></FaLinkedin>,
+      href: "https://www.linkedin.com/in/pawan-kotak-11538916/",
+    },
+    {
+      icon: <FaTwitterSquare></FaTwitterSquare>,
+      href: "https://twitter.com/pawan_kotak",
+    },
+    {
+      icon: <FaRegEnvelope></FaRegEnvelope>,
+      href: "mailto:kotakpawan@gmail.com",
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundImage: `url(${centerimage})` }}>
+      <div className="Container">
+        <header></header>
+        <main>
+          <Slide left>
+            <section className="main-header">
+              <div>
+                <section>{developerName}</section>
+              </div>
+              <div className="subtitle">{developerSubTitle}</div>
+            </section>
+          </Slide>
+          <section className="details">
+            <Slide left>
+              <h5>Skills</h5>
+              <ul>
+                {developerSkills.map((skill, index) => (
+                  <li key={index} data-testid={`skill-test-id-${index}`}>
+                    {skill}
+                  </li>
+                ))}
+              </ul>
+            </Slide>
+          </section>
+          <section className="social-area">
+            <Slide top>
+              <ul>
+                {developerSocials.map((social, index) => (
+                  <li key={index} data-testid={`social-test-id-${index}`}>
+                    <a href={social.href} target="_blank">
+                      {social.icon}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </Slide>
+          </section>
+        </main>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,30 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("App component functionality",()=>{
+
+  test('should render my name', () => {
+    render(<App />);
+    const nameElement = screen.getByText(/Pawan Kotak/i);
+    expect(nameElement).toBeInTheDocument();
+
+  });
+  
+  test('should render skills header',() =>{
+    render(<App></App>);
+    const skillsElement = screen.getByRole("heading",  {name: "Skills"});
+    expect(skillsElement).toBeInTheDocument();
+  });
+  
+  test('should render skill item', () =>{
+    render(<App></App>);
+    const skillTestElement = screen.getByTestId('skill-test-id-0');
+    expect(skillTestElement).toBeInTheDocument();
+  });
+
+  test('should render social item', () => {
+    render(<App></App>);
+    const socialTestElement = screen.getByTestId("social-test-id-0");
+    expect(socialTestElement).toBeInTheDocument();
+  });
+})
